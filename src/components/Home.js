203b3ceb8308ee7bfd1d1d6141b1d3ec22e7fd08
1010varun <varunagarwal10.2@gmail.com>
2023-05-23 16:20:48 +0530

    updated home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div className="mt-60">
-            Home page
-        </div>
-    )
+      <div className="flex justify-center items-center h-screen">
+        <div className="font-extrabold text-3xl">PROTECTED DATA</div>
+      </div>
+    );
 };
 
 export default Home
